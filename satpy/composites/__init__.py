8418c4687127a86c59d62b2f251b300f6ae963ea
Trygve Aspenes <trygve.aspenes@met.no>
2017-10-27 08:55:44 +0000

    satpy/composites/__init__.py add Datasetscompositor

--- satpy/composites/__init__.py
+++ satpy/composites/__init__.py
@@ -826,6 +826,44 @@ class RealisticColors(RGBCompositor):
             raise IncompatibleAreas
         return res
 
+class DatasetsCompositor(CompositeBase):
+
+    def __call__(self, projectables, nonprojectables=None, **info):
+
+        try:
+            the_data = np.rollaxis(
+                np.ma.dstack([projectable for projectable in projectables]),
+                axis=2)
+        except ValueError:
+            raise IncompatibleAreas
+        else:
+            areas = [projectable.info.get('area', None)
+                     for projectable in projectables]
+            areas = [area for area in areas if area is not None]
+            if areas and areas.count(areas[0]) != len(areas):
+                raise IncompatibleAreas
+
+        info = combine_info(*projectables)
+        info.update(self.info)
+        # FIXME: should this be done here ?
+        info["wavelength"] = None
+        info.pop("units", None)
+        sensor = set()
+        for projectable in projectables:
+            current_sensor = projectable.info.get("sensor", None)
+            if current_sensor:
+                if isinstance(current_sensor, (str, bytes, six.text_type)):
+                    sensor.add(current_sensor)
+                else:
+                    sensor |= current_sensor
+        if len(sensor) == 0:
+            sensor = None
+        elif len(sensor) == 1:
+            sensor = list(sensor)[0]
+        info["sensor"] = sensor
+        info["mode"] = "Datasets"
+        return Dataset(data=the_data, **info)
+
 
 def enhance2dataset(dset):
     """Apply enhancements to dataset *dset* and convert the image data
